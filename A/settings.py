3a1a1eceb51154a252cfea26458cc1bd9e5e65ad
mahdi <mahdiamr83@gmail.com>
2024-11-18 19:07:32 +0330

    database

--- A/settings.py
+++ A/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'A.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# for postgresql ==> pip install psycopg
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test',
+        'USER': 'postgres',
+        'PASSWORD': '#####',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
