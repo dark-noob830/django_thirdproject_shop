0496c69d08eb8e2d36eb1d999ca9875ccf7e28ef
mahdi <mahdiamr83@gmail.com>
2024-11-18 17:18:28 +0330

    permissions and group

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,32 +2,43 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import Group
-from .models import User , OtpCode
+from .models import User, OtpCode
 
 
 @admin.register(OtpCode)
 class OtpCodeAdmin(admin.ModelAdmin):
-    list_display = ['phone_number','code','created_at']
+    list_display = ['phone_number', 'code', 'created_at']
 
 
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
 
-    list_display = ['email', 'phone_number', 'is_admin']
-    list_filter = ['is_admin']
+    list_display = ('email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    readonly_fields = ('last_login',)
+
     fieldsets = (
-        (None,{'fields':['email','phone_number','full_name','password']}),
-        ('Permissions',{'fields':['is_active','is_admin','last_login']}),
+        ('Main', {'fields': ('email', 'phone_number', 'full_name', 'password')}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_admin', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
     )
 
     add_fieldsets = (
-        (None, {'fields': ('email','phone_number','full_name','password1','password2')}),
+        (None, {'fields': ('phone_number', 'email', 'full_name', 'password1', 'password2')}),
     )
-    search_fields = ["email",'full_name']
-    ordering = ['full_name']
-    filter_horizontal = ()
 
-admin.site.unregister(Group)
+    search_fields = ('email', 'full_name')
+    ordering = ('full_name',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
+
+
+# admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
-# Register your models here.
